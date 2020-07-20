4d9a57d106d5801de666bc32e8d05be4bb58d885
ERIC WANG <ericw.1991@0wang.,ail>
2020-07-20 09:05:44 +0800

    deite server

--- server/index.js
+++ server/index.js
@@ -4,29 +4,29 @@ const Koa = require("koa");
 const http = require("http");
 
 // Initialize NextJs instance and expose request handler
-// const dev = process.env.NODE_ENV !== "production"; //determine whether it is in dev: developer state or production: official service state
-// const app = next({ dev }); //Initialize next.js
-// const handle = app.getRequestHandler(); // get http response
+const dev = process.env.NODE_ENV !== "production"; //determine whether it is in dev: developer state or production: official service state
+const app = next({ dev }); //Initialize next.js
+const handle = app.getRequestHandler(); // get http response
 
 const PORT = process.env.PORT || 3000;
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/plain");
-	res.end("../pages/index.js\n");
-});
-
-// app.prepare().then(() => {
-// 	//Initialize KoaJs server and router
-// 	const server = new Koa();
-// 	server.use(async (ctx, next) => {
-// 		await handle(ctx.req, ctx.res);
-// 		ctx.respond = false;
-// 	});
-
-// 	server.listen(PORT, () => {
-// 		console.log(`koa server listening on ${PORT}`);
-// 	});
+// const server = http.createServer((req, res) => {
+// 	res.statusCode = 200;
+// 	res.setHeader("Content-Type", "text/plain");
+// 	res.end("../pages/index.js\n");
 // });
-server.listen(PORT, () => {
-	console.log(`koa server listening on ${PORT}`);
+
+app.prepare().then(() => {
+	//Initialize KoaJs server and router
+	const server = new Koa();
+	server.use(async (ctx, next) => {
+		await handle(ctx.req, ctx.res);
+		ctx.respond = false;
+	});
+
+	server.listen(PORT, () => {
+		console.log(`koa server listening on ${PORT}`);
+	});
 });
+// server.listen(PORT, () => {
+// 	console.log(`koa server listening on ${PORT}`);
+// });
